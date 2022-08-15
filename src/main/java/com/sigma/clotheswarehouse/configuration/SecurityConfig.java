6c03651871f0b2c28dde1fccfbd9e70d375bf8c0
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2022-08-15 13:04:46 +0500

    cors configuration

--- src/main/java/com/sigma/clotheswarehouse/configuration/SecurityConfig.java
+++ src/main/java/com/sigma/clotheswarehouse/configuration/SecurityConfig.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -40,17 +40,9 @@ public class SecurityConfig {
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors()
-                .configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
                 .and()
-                .csrf()
-                .disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .formLogin()
-                .disable()
-                .httpBasic()
-                .disable()
+//                .csrf()
+//                .disable()
                 .authorizeHttpRequests(authorize -> authorize
                         .antMatchers("/api/**","/swagger-resources/**",
                                 "/swagger-ui/**",
@@ -71,5 +63,19 @@ public class SecurityConfig {
                 )
                 .formLogin(withDefaults());
         return http.build();
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET, POST", "PUT", "PATCH", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
