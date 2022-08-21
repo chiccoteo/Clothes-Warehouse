da83f806b762a8172408b20f2b1ea8e5b2190834
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2022-08-21 16:09:34 +0500

    Security defaultSuccess login url

--- src/main/java/com/sigma/clotheswarehouse/configuration/SecurityConfig.java
+++ src/main/java/com/sigma/clotheswarehouse/configuration/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
                         "/swagger-ui/**",
                         "/clothes-warehouse",
                         "/webjars/**")
-                .hasAnyRole("ADMIN")
+                .permitAll()
                 .anyRequest()
                 .authenticated();
         return http.build();
