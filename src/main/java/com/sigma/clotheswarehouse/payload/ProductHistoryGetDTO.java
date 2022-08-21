3841235f2143762465066bd193dd8b57ab89d970
samir <sharipovsamir480@gmail.com>
2022-08-21 13:31:25 +0500

    ProductHistoryDTO bug

--- src/main/java/com/sigma/clotheswarehouse/payload/ProductHistoryGetDTO.java
+++ src/main/java/com/sigma/clotheswarehouse/payload/ProductHistoryGetDTO.java
@@ -5,12 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.security.Timestamp;
+import java.util.UUID;
 
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductHistoryGetDTO {
+    private UUID id;
+
     private ProductGetDto productGetDto;
 
     private double price;
